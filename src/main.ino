d30c8b0439ef5a636a478b6909d298129595e837
Semolik <70706334+Semolik@users.noreply.github.com>
2023-01-18 21:54:50 +0700

    upd

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,10 @@
+#include <Arduino.h>
+#include <GyverMotor.h>
+#include <Thread.h>
+#include <PS2X_lib.h>
+#include <GyverTM1637.h>
+#include <FastLED.h>
+
 // мин. сигнал, при котором мотор начинает вращение
 #define MIN_DUTY 120
 
@@ -12,110 +19,131 @@
 #define PS2_CMD A1
 #define PS2_SEL A2
 #define PS2_CLK A3
+
+// пины дисплея
 #define CLK 12
 #define DIO 11
 
+// параметры светодиодов
+#define NUM_LEDS 5
+#define DATA_PIN 6
+
 volatile boolean LEDHdlts;
-bool low_power = false;
+volatile boolean LED_TEST;
+volatile boolean low_power;
 int battery_pin = A4;
+volatile boolean isStopped;
+unsigned long time;
 
-// ===========================
-
-#include <GyverMotor.h>
-#include <Thread.h>
 // (тип, пин, ШИМ пин, уровень)
 GMotor motorR(DRIVER2WIRE, MOT_RA, MOT_RB, HIGH);
 GMotor motorL(DRIVER2WIRE, MOT_LA, MOT_LB, HIGH);
 
-#include <PS2X_lib.h>
 PS2X ps2x;
 
-#include "GyverTM1637.h"
 GyverTM1637 disp(CLK, DIO);
-Thread mainThread = Thread(); // создаём поток управления светодиодом
+
+Thread mainThread = Thread();
 Thread batteryThread = Thread();
+
+CRGB leds[NUM_LEDS];
 void setup()
 {
-  Serial.begin(9600);
-  mainThread.onRun(car); // назначаем потоку задачу
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  mainThread.onRun(car);
   mainThread.setInterval(20);
 
   batteryThread.onRun(buttory_check);
   batteryThread.setInterval(1000);
+
   disp.clear();
   disp.brightness(7);
 
   motorR.setMode(AUTO);
   motorL.setMode(AUTO);
-
-  // НАПРАВЛЕНИЕ ГУСЕНИЦ (зависит от подключения)
   motorR.setDirection(NORMAL);
   motorL.setDirection(NORMAL);
-
-  // мин. сигнал вращения
   motorR.setMinDuty(MIN_DUTY);
   motorL.setMinDuty(MIN_DUTY);
-
-  // плавность скорости моторов
   motorR.setSmoothSpeed(1000);
   motorL.setSmoothSpeed(1000);
 
   // подрубаем геймпад
   ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, false, false);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  if (battery_percentage() < 10)
-  {
-    low_power = true;
-  }
+  low_power = battery_percentage() < 10;
+
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void car()
 {
+  if ((millis() - time > 10000) && (battery_percentage() < 10))
+  {
+    low_power = true;
+  }
   bool success = ps2x.read_gamepad(false, 0); // читаем
   ps2x.reconfig_gamepad();                    // костыль https://stackoverflow.com/questions/46493222/why-arduino-needs-to-be-restarted-after-ps2-controller-communication-in-arduino
   if (success)
   {
     if (ps2x.ButtonPressed(PSB_GREEN)) // Triangle pressed
     {
-      LEDHdlts = !LEDHdlts; // Toggle the LED light flag
+      LEDHdlts = !LEDHdlts;
     }
-
-    // преобразуем стики от 0..255 к -255, 255
+    if (ps2x.ButtonPressed(PSB_R1)) // X pressed
+    {
+      LED_TEST = !LED_TEST;
+    }
+    if (LED_TEST)
+    {
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Red;
+      }
+      FastLED.show();
+    }
+    else
+    {
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
+        leds[i] = CRGB::Black;
+      }
+      FastLED.show();
+    }
     int LX = map(ps2x.Analog(PSS_RX), 255, 0, -255, 255);
     int LY = map(ps2x.Analog(PSS_LY), 255, 0, -255, 255);
 
     float dutyR = constrain(LX, -255, 255);
     float dutyL = constrain(LY, -255, 255);
-    // задаём целевую скорость
+
     motorR.smoothTick(dutyR == -1 ? 0 : dutyR);
     motorL.smoothTick(dutyL == -1 ? 0 : dutyL);
 
     if (dutyR == -1 && dutyL == -1)
     {
-      disp.displayInt(battery_percentage());
-    }
-    else
-    {
-      Serial.print("asdasd");
+      time = millis();
     }
     digitalWrite(LED_BUILTIN, LEDHdlts);
   }
   else
   {
-    // проблема с геймпадом - остановка
     motorR.setSpeed(0);
     motorL.setSpeed(0);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 void loop()
 {
-
-  if (mainThread.shouldRun())
+  if (!low_power && mainThread.shouldRun())
+  {
     mainThread.run();
+  }
 
   if (batteryThread.shouldRun())
+  {
     batteryThread.run();
+  }
 }
 
 void buttory_check()
@@ -140,8 +168,7 @@ int battery_percentage()
   float voltage = (analogRead(battery_pin) * 5.0) / 1023.0; // for default reference voltage
   // round value by two precision
   voltage = roundf(voltage * 100) / 100;
-  Serial.print("voltage: ");
-  Serial.println(voltage, 2);
+
   output = ((voltage - battery_min) / (battery_max - battery_min)) * 100;
   if (output < 100)
     return output;
